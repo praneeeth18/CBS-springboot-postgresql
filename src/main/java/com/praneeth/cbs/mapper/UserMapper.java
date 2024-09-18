9c88826f6f33fd94dabe6789e01f5b857f20b712
Praneeth Reddy <praneeth608@gmail.com>
2024-09-18 16:31:22 +0530

    User mapper methods added

---
+++ src/main/java/com/praneeth/cbs/mapper/UserMapper.java
@@ -0,0 +1,27 @@
+package com.praneeth.cbs.mapper;
+
+import com.praneeth.cbs.dto.UserRequestDTO;
+import com.praneeth.cbs.dto.UserResponseDTO;
+import com.praneeth.cbs.model.User;
+
+public class UserMapper {
+	
+	public User toUser(UserRequestDTO user) {
+		return User.builder()
+				.firstName(user.firstName())
+				.lastName(user.lastName())
+				.email(user.email())
+				.password(user.password())
+				.build();
+	}
+	
+	public UserResponseDTO toUserDTO(User user) {
+		return new UserResponseDTO(
+				user.getUserId(),
+				user.getFirstName(),
+				user.getLastName(),
+				user.getEmail()
+				);
+	}
+
+}
